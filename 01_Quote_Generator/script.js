562050bac3de296903c6217e3f0e58382eaa2afa
faisal-din <dahrifaisal56@gmail.com>
2024-10-17 23:25:19 +0500

    added infinite scroll project

--- 01_Quote_Generator/script.js
+++ 01_Quote_Generator/script.js
@@ -1,5 +1,3 @@
-// Get Quotes From API
-
 const quoteContainer = document.querySelector('.quote-container');
 const quoteText = document.querySelector('#quote');
 const authorText = document.querySelector('#author');
@@ -7,63 +5,53 @@ const twitterBtn = document.querySelector('#twitter');
 const newQuoteBtn = document.querySelector('#new-quote');
 const loader = document.querySelector('.loader');
 
-let currentQuote  = [];
-
-// Show Loading Spinner 
-function loading() {
-    loader.hidden = false;
-    quoteContainer.hidden = true;
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-// Hide Loading Spinner
-function complete(params) {
-    quoteContainer.hidden = false;
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
     loader.hidden = true;
-    
+    quoteContainer.hidden = false;
+  }
 }
-
 
 // Show New Quote
-function newQuote() {
-    loading();
-    const quote = currentQuote;
-    // Check if Author field is blank and replace it with 'Unknown'
-    if (!quote.author) {
-        authorText.textContent = 'Unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
- 
-    if (quote.quote.length > 120) {
-        quoteText.classList.add('long-quote');
-    } else {
-        quoteText.classList.remove('long-quote');
-    }
-    //Set Quote, Hide Loader
-    quoteText.textContent = quote.quote;
-    complete();
+function newQuote(data) {
+  showLoadingSpinner();
+
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!data[0].author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = data[0].author;
+  }
+
+  if (data[0].quote.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  //Set Quote, Hide Loader
+  quoteText.textContent = data[0].quote;
+  removeLoadingSpinner();
 }
 
 // Get Quotes From API
 async function getQuotes() {
-    loading();
+  showLoadingSpinner();
 
-    const apiUrl = 'https://api.api-ninjas.com/v1/quotes';
-    const apiKey = 'DAHF9GqFyczhxaYKbzJXbw==zpNxK9RUbrIjscgP';
-    
-    const headers = {
-        'X-Api-Key': apiKey,
-    };
+  const apiUrl = 'https://api.api-ninjas.com/v1/quotes';
+  const apiKey = 'DAHF9GqFyczhxaYKbzJXbw==zpNxK9RUbrIjscgP';
+  const headers = {
+    'X-Api-Key': apiKey,
+  };
 
   try {
     const response = await fetch(apiUrl, { headers });
     const data = await response.json();
-        
-    // Check if data is an array and get the first item, or use the object directly
-    currentQuote = Array.isArray(data) ? data[0] : data;
-   
-    
-    newQuote();
+    newQuote(data);
   } catch (error) {
     console.log('Whoops, error', error);
   }
@@ -71,12 +59,13 @@ async function getQuotes() {
 
 // Tweet Quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl, '_blank');
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
 }
 
 // Event Listeners
 newQuoteBtn.addEventListener('click', getQuotes);
-twitterBtn.addEventListener('click', tweetQuote); 
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On Load
 getQuotes();
---
+++ 02_Infinite_Scroll/script.js
@@ -0,0 +1,86 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+// Unsplash API
+const count = 30;
+const apiKey = '0WEmQyoblOizJpWZoOFSwQKVLF_bv7Xe6b_vaoF80SQ';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+  imageLoaded = 0;
+  totalImages = photosArray.length;
+
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+
+    // Create <img> for photo
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    // Event Listener, check when each is finished loading
+    img.addEventListener('load', () => {
+      imageLoaded();
+    });
+
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
+    photosArray = data;
+    displayPhotos();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+getPhotos();
